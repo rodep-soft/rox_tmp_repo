c406f7b37e414a7efa7e46c88d079a7ffd2825f5
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2025-07-12 16:21:40 +0900

    wip

--- ros_ws/src/color_sensor/color_sensor/connectors/i2c.py
+++ ros_ws/src/color_sensor/color_sensor/connectors/i2c.py
@@ -1,5 +1,6 @@
 from smbus2 import SMBus
 
+
 class I2C:
     def __init__(self, bus_number=1, address=0x29):
         self.bus = SMBus(bus_number)
@@ -10,10 +11,10 @@ class I2C:
 
     def write_byte(self, register, value):
         self.bus.write_byte_data(self.address, register, value)
-    
+
     def write_block_data(self, register, datas):
         self.bus.write_i2c_block_data(self.address, register, datas)
-    
+
     def read_block_data(self, register, length):
         return self.bus.read_i2c_block_data(self.address, register, length)
 
--- ros_ws/src/color_sensor/launch/color_sensor.launch.py
+++ ros_ws/src/color_sensor/launch/color_sensor.launch.py
@@ -1,14 +1,15 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     ld = LaunchDescription()
-    
+
     node = Node(
-        package='color_sensor',
-        executable='color_publisher',
-        name='color_publisher_node'
+        package="color_sensor",
+        executable="color_publisher",
+        name="color_publisher_node",
     )
-    
+
     ld.add_action(node)
     return ld
