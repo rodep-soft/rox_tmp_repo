58b18d446ad75bdfd23d7c70a75e46fc23799cc2
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2025-07-15 17:15:36 +0900

    Automated by script

--- ros_ws/src/lifting_motor/lifting_motor/lifting_motor.py
+++ ros_ws/src/lifting_motor/lifting_motor/lifting_motor.py
@@ -3,31 +3,35 @@ from time import sleep
 import rclpy
 from gpiozero import Motor
 from rclpy.node import Node
-from sensor_msgs.msg import Joy
+from std_msgs.msg import Bool
 
 
 class LiftingMotorNode(Node):
-
     def __init__(self):
         super().__init__("lifting_motor_node")
-        self.subscription = self.create_subscription(Joy, "/joy", self.joy_callback, 3)
-        self.subscription
+        self.subscription = self.create_subscription(Bool, "/upper_on", self.motor_callback, 1)
+        self.motor_on = False
+        self.motor = Motor(forward=17, backward=27, enable=18)
 
-    def joy_callback(self, msg):
-        motor = Motor(forward=17, backward=27, enable=18)
-        motor.forward(speed=msg)
+    def motor_callback(self, msg):
+        # msg is of type std_msgs.msg.Bool
+        if msg.data:
+            self.motor_on = not self.motor_on
+            if self.motor_on:
+                self.get_logger().info("Motor ON")
+                self.motor.forward(speed=1.0)
+            else:
+                self.get_logger().info("Motor OFF")
+                self.motor.stop()
 
 
 def main(args=None):
     rclpy.init(args=args)
-
     node = LiftingMotorNode()
-
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
 
 
 if __name__ == "__main__":
-    rclpy.init()
     main()
