a4bee106f7c99152c161ca486a46a1c24349127f
zyodooon <ponqoo@outlook.com>
2025-07-17 20:20:52 +0900

    修正と角速度を速度m/sに直すところを追加

--- sandbox/mecanum_speed.cpp
+++ sandbox/mecanum_speed.cpp
@@ -2,32 +2,36 @@
 #include <cmath>
 #include <Eigen/Dense>
 
-const int WHIDE = 0.40;
-const int LENGTH = 0.40;
-const int WHEEL_RADIUS = 0.02;
+const float WIDE = 0.40;
+const float LENGTH = 0.40;
+const float WHEEL_RADIUS = 0.02;
 
 int main(){
     //それぞれのrpmm
-    float rpm[4] = {0.0, 0.0, 0.0, 0.0};//[FL, FR, RL, RR]  rotation/min
+    float rpm[4] = {40.0, 40.0, 40.0, 40.0};//[FL, FR, RL, RR]  rotation/min
 
-    //rpmをrad/sに変換
-    
-    Eigen::Vector4f rad_s;
+    Eigen::Vector4f rad_s;//  rad/s
+    Eigen::Vector4f speed_m_s;//  m/s
+
     for(int i = 0;i < 4;i++){
-        rad_s[i] = rpm[i] * 2 * M_PI /60.0;
+        rad_s[i] = rpm[i] * 2 * M_PI /60.0;// 角速度(rad/s)に変換
+        speed_m_s[i] = rad_s[i] * WHEEL_RADIUS;// 角速度→線速度(m/s)に変換
     }
 
-    Eigen::Matrix4f A;
-    A << 1, -1, -(WHIDE + LENGTH)/2,
-        1, 1, -(WHIDE + LENGTH)/2,
-        1, -1, (WHIDE + LENGTH)/2,
-        1, 1, (WHIDE + LENGTH)/2;
+    
+    Eigen::Matrix<float, 4, 3> A;
+    A << 1, -1, -(WIDE + LENGTH)/2,
+        1, 1, -(WIDE + LENGTH)/2,
+        1, -1, (WIDE + LENGTH)/2,
+        1, 1, (WIDE + LENGTH)/2;
 
-    Eigen::Matrix3f A_inverce = (A.transpose() * A).inverse() * A.transpose();
-    Eigen::Vector3f speed = A_inverce * rad_s;
+    // Aの逆行列を計算
+    Eigen::Matrix3f A_inverse = (A.transpose() * A).inverse() * A.transpose();
+    Eigen::Vector3f speed = A_inverse * rad_s;
 
-    std::cout << "vx = "<< speed[0] << " m/s" << std::endl;
-    std::cout << "vy = "<< speed[1] << " m/s" << std::endl;
-    std::cout << "wz = "<< speed[2] << " rad/s" << std::endl;
+    std::cout << "vx = "<< speed[0] << " m/s" << std::endl;//x方向の速度
+    std::cout << "vy = "<< speed[1] << " m/s" << std::endl;// y方向の速度
+    std::cout << "wz = "<< speed[2] << " rad/s" << std::endl;// z軸周りの角速度
 
+    return 0;
 }
