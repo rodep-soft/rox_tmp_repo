eb9105b9305294c08883363cc8caca740b2514e1
zyodooon <ponqoo@outlook.com>
2025-07-25 16:40:18 +0900

    wid

--- ros_ws/src/joy_driver/src/joy_driver_node.cpp
+++ ros_ws/src/joy_driver/src/joy_driver_node.cpp
@@ -130,30 +130,26 @@ class JoyDriverNode : public rclcpp::Node {
     upper_msg->drive = msg->buttons[1];
     upper_msg->stop = msg->buttons[2];
 
-    void joy_rotation(){
-      if(msg->axes[5] >= 0.95){
-        // L2の右旋回
-        twist_msg.linear.x = 0.0;
-        twist_msg.linear.y = 0.0;
-        msg->axes[5] = 1.0;
-        this->twist_msg.angular.z =  (msg->axes[4]-1)/2.0;
-      } else if(msg->axes[4] >= 0.95){
-        // R2の左旋回
-        twist_msg.linear.x = 0.0;
-        twist_msg.linear.y = 0.0;
-        msg->axes[4] = 1.0;
-        this->twist_msg.angular.z = -(msg->axes[5]-1)/2.0;
-      }
-    }
-
-
-
     //RCLCPP_INFO(this->get_logger(), "linear.x=%.2f, linear.y=%.2f, angular.z=%.2f",
     //            twist_msg.linear.x, twist_msg.linear.y, twist_msg.angular.z);
 
     upper_publisher_->publish(std::move(upper_msg));
     cmd_vel_publisher_->publish(twist_msg);
     cmd_dpad_publisher_->publish(std::move(dpad_msg));
+  }
+
+  void joy_rotation(){
+      if(msg->axes[5] >= 0.95){
+        // L2の右旋回
+        twist_msg.linear.x = 0.0;
+        twist_msg.linear.y = 0.0;
+        twist_msg.angular.z =  (msg->axes[4]-1)/2.0;
+      } else if(msg->axes[4] >= 0.95){
+        // R2の左旋回
+        twist_msg.linear.x = 0.0;
+        twist_msg.linear.y = 0.0;
+        twist_msg.angular.z = -(msg->axes[5]-1)/2.0;
+      }
   }
 
   // ROS 2 components
