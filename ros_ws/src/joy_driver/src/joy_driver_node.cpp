4aaaa28d716e8b2584ddd11c5a93f8f190e8e6f8
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2025-08-08 21:46:19 +0900

    wip

--- ros_ws/src/joy_driver/src/joy_driver_node.cpp
+++ ros_ws/src/joy_driver/src/joy_driver_node.cpp
@@ -407,3 +407,5 @@ int main(int argc, char** argv) {
   rclcpp::shutdown();
   return 0;
 }
+
+
--- ros_ws/src/lifting_motor/lifting_motor/lifting_motor.py
+++ ros_ws/src/lifting_motor/lifting_motor/lifting_motor.py
@@ -6,6 +6,12 @@ from gpiozero import Motor, OutputDevice, Button
 from rclpy.node import Node
 from custom_interfaces.msg import UpperMotor
 
+# 押出の状態を定義
+class State(Enum):
+    INIT = 0
+    STOPPED = 1
+    TO_MAX = 2
+    RETURN_TO_MIN = 3
 class LiftingMotorNode(Node):
 
     def __init__(self):
@@ -43,13 +49,6 @@ class LiftingMotorNode(Node):
         self.elevation_motor = Motor(forward=self.elevation_motor_forward_pin, backward=self.elevation_motor_backward_pin, enable=self.elevation_motor_enable_pin)
         self.throwing_motor = OutputDevice(self.relay_pin, active_high=True, initial_value=False)
 
-        # 押出の状態を定義
-        class State(Enum):
-            INIT = 0
-            STOPPED = 1
-            TO_MAX = 2
-            RETURN_TO_MIN = 3
-
         # 初期状態はINIT
         self.current_state = State.INIT
 
@@ -60,9 +59,9 @@ class LiftingMotorNode(Node):
         self.is_elevation_minlim_on = False
 
         # モーターの状態管理用のフラグ
-        self.is_throwing_on = False
-        self.is_ejection_on = False
-        self.is_elevation_on = False
+        self.is_throwing_motor_on = False
+        self.is_ejection_motor_on = False
+        self.is_elevation_motor_on = False
 
 
     # Callback function for UpperMotor messages
@@ -70,6 +69,7 @@ class LiftingMotorNode(Node):
     def motor_callback(self, msg):
 
         self.detect_sw_state() # 全リミットスイッチの状態を検出
+        self.detect_motor_state() # 全モーターの状態を検出
 
         # # 射出モーター (リレー駆動)
         # if msg.is_throwing_on == 1:
@@ -87,40 +87,47 @@ class LiftingMotorNode(Node):
         # else:
         #     self.elevation_motor.stop()
 
+
         # 状態遷移のロジック
         if self.current_state == self.State.INIT and \
            msg.something_on == True: # これは本当の名前に置き換える
-            self.current_state == self.State.STOPPED
+            self.current_state = self.State.STOPPED
         elif self.current_state == self.State.STOPPED and \
-           msg.is_throwing_on == True and \
+           self.is_throwing_motor_on == True and \
            self.is_elevation_minlim_on == True and \
-           self.is_ejection_on == True:
+           msg.is_ejection_on == True:
+            # 射出モーターがONで、昇降が完全に下がっていて、押し出しがONになったとき遷移
             self.current_state = self.State.TO_MAX #状態遷移
         elif self.current_state == self.State.TO_MAX and \
-            msg.is_ejection_maxmlim_on == True:
+            self.is_ejection_maxlim_on == True:
+            # 押し出しが最大まで伸び切ってリミットスイッチを押したとき、遷移
             self.current_state = self.State.RETURN_TO_MIN #状態遷移
             # Action:
-            self.relay_motor.off() # 押し出しが完了したらリレーをオフにする     
-        elif self.current_state == self.State.RETURN_TO_MIN and msg.is_ejection_minlim_on == True:
+            self.throwing_motor.off() # 押し出しが完了したらリレーをオフにする     
+        elif self.current_state == self.State.RETURN_TO_MIN and \
+            self.is_ejection_minlim_on == True:
+            # 押し出しが完全に引っ込んで最小リミットスイッチを押したら遷移
             self.current_state = self.State.STOPPED #状態遷移
 
         # 押出モーターの実際の動作
         if self.current_state == self.State.STOPPED:
-            self.ejection_motor.stop()
-            self.relay_on()
+            if self.is_ejection_motor_on == True:
+                self.ejection_motor.stop()
+            self.relay_on(msg)
         elif self.current_state == self.State.TO_MAX:
             self.ejection_motor.forward(speed=1.0)
         elif self.current_state == self.State.RETURN_TO_MIN:
             self.ejection_motor.backward(speed=1.0)
-            self.relay_on()
+            self.relay_on(msg)
 
 
-    def relay_on(self):
+    def relay_on(self, msg):
         # リレーはSTOPPEDまたはRETURN_TO_MIN状態のときとか駆動することはできない
-        if self.is_throwing_on == True:
-            self.relay_motor.on()
+        if msg.is_throwing_on == True and self.is_throwing_motor_on == False:
+            self.throwing_motor.on()
         # else:
         #     self.relay_motor.off()
+
 
     # リミットスイッチの状態を検出するメソッド
     def detect_sw_state(self):
@@ -140,17 +147,20 @@ class LiftingMotorNode(Node):
             self.get_logger().info("Elevation Min Limit Switch is pressed")
             self.is_elevation_minlim_on = True
 
+
     # モーターの状態を検出するメソッド
     # モーターが動作しているかどうかを確認する　
     def detect_motor_state(self):
         if self.throwing_motor.is_active == True:
-            self.is_throwing_on = True
+            self.is_throwing_motor_on = True
         
+        # は？
         if self.ejection_motor.is_active == True:
-            self.is_ejection_on = True
+            self.is_ejection_motor_on = True
 
+        # は??
         if self.elevation_motor.is_active == True:
-            self.is_elevation_on = True
+            self.is_elevation_motor_on = True
 
 
 
