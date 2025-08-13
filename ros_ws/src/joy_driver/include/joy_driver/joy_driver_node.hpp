7f6bd10d0f62b7bc75ac530316dae1ac6b094f16
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2025-08-13 22:07:12 +0900

    #update break joy_driver.cpp into hpp and cpp

---
+++ ros_ws/src/joy_driver/include/joy_driver/joy_driver_node.hpp
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <memory>
+#include <string>
+
+// include ROS2
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/qos.hpp>
+
+
+// ROS messages
+#include <geometry_msgs/msg/twist.hpp>
+#include <sensor_msgs/msg/joy.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <std_msgs/msg/string.hpp>
+#include "custom_interfaces/msg/cmd_dpad.hpp"
+#include "custom_interfaces/msg/upper_motor.hpp"
+
+// ROS services
+#include <std_srvs/srv/set_bool.hpp>
+
+class JoyDriverNode : public rclcpp::Node {
+    public:
+        // Constructor
+        JoyDriverNode();
+
+    private:
+        // 足回りのモードを定義
+        enum class Mode { STOP, JOY, DPAD, LINETRACE };
+
+        // default Mode
+        Mode mode_ = Mode::STOP;
+
+        // ros2パラメータを宣言、取得する関数
+        void declare_parameters();
+        void get_parameters();
+        
+        // Callback functions
+        void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
+        void rpy_callback(const geometry_msgs::msg::Vector3::SharedPtr msg);
+
+        // Helper functions
+        double get_angular_velocity(const sensor_msgs::msg::Joy::SharedPtr& msg);
+        static double applyDeadzone(double val, double threshold = 0.05);
+        std::string mode_to_string(Mode mode);
+
+
+        // ===== ROS2 components =====
+
+        // ROS2 Subscription
+        rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_subscription_;
+        rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr rpy_subscription_;
+
+        // ROS2 Publisher
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
+        rclcpp::Publisher<custom_interfaces::msg::UpperMotor>::SharedPtr upper_publisher_;
+        rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr linetrace_publisher_;
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr mode_publisher_;
+
+        // ROS2 Client
+        rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr brake_client_;
+
+        // ros2 QoS
+        const rclcpp::QoS reliable_qos = rclcpp::QoS(1).reliable();
+        const rclcpp::QoS best_effort_qos = rclcpp::QoS(10).best_effort();
+
+        // ===== ROS2 components END =====
+
+        // ===== ros2 params =====
+
+        // joystick params
+        double linear_x_scale_;
+        double linear_y_scale_;
+        double angular_scale_;
+        int linear_x_axis_;
+        int linear_y_axis_;
+        int angular_axis_;
+
+        // PID 
+        double Kp:
+        double deadband_;
+        double max_angular_correction_;
+
+        // ===== ros2 params END =====
+
+        // Euler
+        double pitch_ = 0.0;
+        double roll_ = 0.0;
+        double yaw_ = 0.0;
+
+        // 初期化時のyaw値
+        // これはロボットの初期姿勢を基準にするための値
+        // これを用いてDPADモードでの直線移動での回転方向のズレを補正する
+        // L2, R2を押した時も値を更新する
+        double init_yaw_ = 0.0;
+
+        // member constants
+        const double TRIGGER_THRESHOLD = 0.95;
+
+        // other member variables (ex. flags)
+        bool prev_linetrace_buttons_ = false;
+        bool reverse_flag = false;
+        bool prev_reverse_button = 0;
+
+        bool prev_throwing_on = false;
+        bool prev_ejection_on = false;
+        bool prev_elevation_on = false;
+
+
+};
