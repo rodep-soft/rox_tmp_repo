2a7215b8ba58a22be4e6a12bbcfddfaa066fddde
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2025-08-16 04:58:22 +0900

    ledを変更

---
+++ ros_ws/src/led_control/led_control/led_scripts/violet.py
@@ -0,0 +1,29 @@
+import board
+import neopixel
+import time
+import fcntl
+
+
+# # while True:
+# pixels.fill((0, 255, 0))  # 赤
+# pixels.show()
+
+LOCK_FILE = '/tmp/led.lock'
+
+def main():
+    with open(LOCK_FILE, 'w') as lockfile:
+        fcntl.flock(lockfile, fcntl.LOCK_EX)
+        try:
+            pixels = neopixel.NeoPixel(board.D23, 6, brightness=1.0, auto_write=False, pixel_order=neopixel.RGB)
+            pixels.fill((0, 255, 255))
+            pixels.show()
+            while True:
+                time.sleep(1)
+        finally:
+            pixels.fill((0, 0, 0))
+            pixels.show()
+            fcntl.flock(lockfile, fcntl.LOCK_UN)
+
+
+if __name__ == "__main__":
+    main()
